17aa9556dafd4a3a8c407b9f9c936cc153acdc50
Sébastien RONDEAU-CAMEIRA <2760742+noahseed@users.noreply.github.com>
2019-01-20 17:06:44 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="keywords" content="rondeau-cameira, rondeau, cameira, sebastien, sébastien">
+    <meta name="robots" content="all">
+    <meta name="Content-language" content="french">
+    <meta name="Rating" content="General">
+    <meta name="revisit-after" content="1 month">
+    <meta name="author" content="Sébastien RONDEAU-CAMEIRA">
+    <title>rondeau-cameira.fr</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    <link rel="icon" type="image/png" href="favicon.png">
+</head>
+<body>
+    <main id="text">
+        <h1>rondeau-cameira.fr</h1>
+    </main>
+</body>
+</html>
