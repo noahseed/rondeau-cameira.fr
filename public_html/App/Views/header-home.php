05a97843374835922a929b652f4598404d5b600b
Sébastien <2760742+noahseed@users.noreply.github.com>
2021-10-31 19:49:27 +0100

    Initial commit

--- public_html/App/Views/header-home.php
+++ public_html/App/Views/header-home.php
@@ -1,2 +1,2 @@
-<a href="https://links.rondeau-cameira.fr/"><img src="img/aum_white.png" alt="" id="logo" class="big-logo"></a>
-<h1 class="big-title">rondeau-cameira.fr</h1>
+<a href="sebastien/"><img src="img/aum_white.png" alt="" id="logo" class="big-logo"></a>
+<h1 class="big-title">BIENVENUE ! 😊</h1>
